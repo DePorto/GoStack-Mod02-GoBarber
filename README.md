803c1e6908f998965554d00b6eb5cd73774283db
Deini Porto <dasporto@yahoo.com.br>
2020-03-30 11:35:58 -0300

    ajuste

--- README.md
+++
@@ -1,3 +0,0 @@
-#GoStack-Mod02-GoBarber
-
-Arquivos do App GoBarber do Bootcamp GoStack da RocketSeat
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import UserController from './app/controllers/UserController';
-
-const { Router } = require('express');
-
-const routes = new Router();
-
-routes.post('/users', UserController.store);
-
-export default routes;
