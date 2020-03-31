9e8bdc25085d76ffa80346367101a2cc9a38b08b
Deini Porto <dasporto@yahoo.com.br>
2020-03-30 23:08:01 -0300

    final das aulas do modulo 2

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0b3ddc11780f47113eec886e183835bd',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import UserController from './app/controllers/UserController';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+// apenas as rotas a seguir passam por esse
+// middleware de verificação de autenticação
+
+routes.put('/users', UserController.update);
 
 export default routes;
