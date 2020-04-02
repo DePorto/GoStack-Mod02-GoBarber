726e7699e056e588dde52fe997619f62283aff1f
Deini Porto <dasporto@yahoo.com.br>
2020-04-02 00:17:19 -0300

    contiuado API gobarber, ate video validaçoes e agendamentos

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
-import './databse';
+import './database';
 
 class App {
   constructor() {
@@ -13,6 +14,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
